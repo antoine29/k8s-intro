68d8ae542241c42ac58dfceb172fdd2dbc860af9
antoine <anthonyfg9@gmail.com>
2023-10-12 14:15:33 -0400

    adds api proj
    updates readme
    
    deletes pong controller
    
    fixes go mod version

---
+++ api-example/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+)
+
+type response struct {
+	Message string `json:"message"`
+	Code    int    `json:"code"`
+	ApiVersion    string    `json:"api_version"`
+}
+
+func main() {
+	port, envVarIsSet := os.LookupEnv("PORT")
+	if !envVarIsSet {
+		slog.Info("PORT env var was not set. Using 8080 as default.")
+		port = "8080"
+	}
+
+	http.HandleFunc("/api/ping", func(w http.ResponseWriter, req *http.Request) {
+    slog.Info("ping received")
+		json_response, _ := json.Marshal(response{
+			Message: "pong",
+			Code:    http.StatusOK,
+      ApiVersion: "v1",
+		})
+
+		w.Write(json_response)
+	})
+
+	slog.Info(fmt.Sprintf("Listening on: %s \n", port))
+	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+}
