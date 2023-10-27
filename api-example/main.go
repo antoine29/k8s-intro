54e12d01854078a56539bf0a977d6de63967e868
anthony <anthony@mail.com>
2023-10-26 21:33:28 -0400

    adds api version to api resp

--- api-example/main.go
+++ api-example/main.go
@@ -9,9 +9,9 @@ import (
 )
 
 type response struct {
-	Message string `json:"message"`
-	Code    int    `json:"code"`
-	ApiVersion    string    `json:"api_version"`
+	Message    string `json:"message"`
+	Code       int    `json:"code"`
+	ApiVersion string `json:"api_version"`
 }
 
 func main() {
@@ -22,27 +22,27 @@ func main() {
 	}
 
 	http.HandleFunc("/api/ping", func(w http.ResponseWriter, req *http.Request) {
-    slog.Info("ping received")
+		slog.Info("ping received")
 		json_response, _ := json.Marshal(response{
-			Message: "pong",
-			Code:    http.StatusOK,
-      ApiVersion: "v3",
+			Message:    "pong",
+			Code:       http.StatusOK,
+			ApiVersion: "v3",
 		})
 
 		w.Write(json_response)
 	})
-	
-  http.HandleFunc("/api/pong", func(w http.ResponseWriter, req *http.Request) {
-    slog.Info("pong received")
-    json_response, _ := json.Marshal(response{
-			Message: "ping",
-			Code:    http.StatusOK,
-      ApiVersion: "v3",
+
+	http.HandleFunc("/api/pong", func(w http.ResponseWriter, req *http.Request) {
+		slog.Info("pong received")
+		json_response, _ := json.Marshal(response{
+			Message:    "ping",
+			Code:       http.StatusOK,
+			ApiVersion: "v3",
 		})
 
 		w.Write(json_response)
 	})
-	
+
 	slog.Info(fmt.Sprintf("Listening on: %s \n", port))
 	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 }
